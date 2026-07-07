966a37cd6ade1aa43e75562a73923d60c4742d42
Mehdy LERETRIF <mehdylrt@proton.me>
2026-07-07 10:22:08 +0200

    Fix Firefly user permission in script
    Set COMPOSER_ALLOW_SUPERUSER environment variable.

--- Setup/setup_fireflyIII.sh
+++ Setup/setup_fireflyIII.sh
@@ -162,6 +162,7 @@ cd /tmp
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm composer-setup.php
+export COMPOSER_ALLOW_SUPERUSER=1
 
 # Verify Composer
 composer --version
